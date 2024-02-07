d7a0d18cbb31df432e4b96b26c4b9600170e29ce
Pavel <braurs64@gmail.com>
2024-02-07 10:42:16 +0400

    fix: bot writes to chat topic

--- netlify/functions/sendFeedback.js
+++ netlify/functions/sendFeedback.js
@@ -1,36 +1,46 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.handler = async function (event) {
   let sendInfo;
   try {
     sendInfo = JSON.parse(event.body);
     if (!sendInfo) {
-      throw 'error';
+      throw "error";
     }
   } catch {
     return {
       statusCode: 200,
-      body: JSON.stringify({ status: 'error' }),
+      body: JSON.stringify({ status: "error" }),
     };
   }
 
   if (Object.values(sendInfo).length < 3) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ status: 'error' }),
+      body: JSON.stringify({ status: "error" }),
     };
   }
   const { name, country, message, email } = sendInfo;
 
-  const { VITE_API_TELEGRAM_TOKEN, VITE_GROUP_TELEGRAM } = process.env;
+  const {
+    VITE_API_TELEGRAM_TOKEN,
+    VITE_GROUP_TELEGRAM,
+    VITE_MESSAGE_THREAD_ID,
+  } = process.env;
 
-  await axios.get(
-    `https://api.telegram.org/bot${VITE_API_TELEGRAM_TOKEN}/sendMessage?text=${encodeURI(
-      `Name: ${name}\nEmail: ${email}\nCountry: ${country}\nText: ${message}&chat_id=${VITE_GROUP_TELEGRAM}`
-    )}`
-  );
+  const telegramURL = VITE_MESSAGE_THREAD_ID
+    ? `https://api.telegram.org/bot${VITE_API_TELEGRAM_TOKEN}/sendMessage?text=${encodeURI(
+        `Name: ${name}\nEmail: ${email}\nCountry: ${country}\nText: ${message}`
+      )}&chat_id=${VITE_GROUP_TELEGRAM}&message_thread_id=${VITE_MESSAGE_THREAD_ID}`
+    : `https://api.telegram.org/bot${VITE_API_TELEGRAM_TOKEN}/sendMessage?text=${encodeURI(
+        `Name: ${name}\nEmail: ${email}\nCountry: ${country}\nText: ${message}`
+      )}&chat_id=${VITE_GROUP_TELEGRAM}`;
+
+  await axios.get(telegramURL);
+  console.log(telegramURL);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ status: 'ok' }),
+    body: JSON.stringify({ status: "ok" }),
   };
 };
